6b0c032bfc81f060fa7547db9e91137d6d97cde3
Germain Gadel <19751200+Germain-Gadel@users.noreply.github.com>
2022-08-16 15:14:38 +0200

    [+] changed planck keymap

--- keyboards/planck/keymaps/gpem/keymap.c
+++ keyboards/planck/keymaps/gpem/keymap.c
@@ -29,6 +29,7 @@ enum layers {
 #define F_SYM LT(_SYM, KC_ESC)
 #define F_NUM LT(_NUM, KC_TAB)
 #define F_ARR MO(_ARROWS)
+#define F_ROW MO(_FROW)
 #define F_MED LT(_MEDIA, KC_ENT)
 #define SFT_Z LSFT_T(KC_Z)
 #define SFT_BC LSFT_T(KC_LBRC)
@@ -48,20 +49,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |   Q  |   W  |   E  |   R  |   T  |      |      |   Y  |   U  |   I  |   O  |   P  |
+ * |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |      |      |   H  |   J  |   K  |   L  |   ;  |
+ * |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |   \  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |      |      |   N  |   M  |   /  |  Up  |SEnter|
+ * |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  Up  |SEnter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Gui  | Alt  | Numb |Space | Symb | Bksp | Bksp | Left | Left | Down |Right |
+ * |      | Ctrl |F_SYM |F_NUM | Space| Bksp | RAlt |F_ROW |F_MED | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______, KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______, KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,
-    SFT_Z,   CTL_X,   LGUIT_C, ALT_V,   KC_B,    _______, _______, KC_N,    ALT_M,   KC_SLSH, KC_UP,    KC_SFTENT,
-    KC_LCTL, KC_LGUI, KC_LALT, F_NUM,   KC_SPC,   F_SYM,   KC_BSPC, KC_BSPC, F_MED,   KC_LEFT, KC_DOWN,  KC_RGHT
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,      KC_U,    KC_I,     KC_O,    KC_P,     SFT_BC,  CTL_BC,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,      KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT, KC_BSLS,
+    SFT_Z,   CTL_X,   LGUIT_C, ALT_V,   KC_B,    KC_N,      ALT_M,   GUI_COMM, CLT_DOT, SFT_SLH,  KC_UP,   KC_SFTENT,
+    _______, KC_LCTL, F_SYM,   F_NUM,,  KC_SPC,  KC_BSPC,   KC_RALT, F_FROW,   F_MED,   KC_LEFT,  KC_DOWN, KC_RGHT
 ),
 
 /* Numbers
@@ -70,16 +71,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|------+------|------+------+------+------+------|
  * |      |   4  |   5  |   6  |   =  |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   1  |   2  |   3  |   -  |      |      |  F11 | AF12 |      |      |      |
+ * |      |   1  |   2  |   3  |   -  |      |      |  F11 |  F12 |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |   .  |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+ * |      |   0  |   .  |      |      |      |      |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUM] = LAYOUT_planck_grid(
     KC_0,     KC_7,  KC_8,  KC_9,   KC_TRNS,   _______, _______,  KC_F1,  KC_F2,   KC_F3,  KC_F4,   KC_F5,
     KC_TRNS,  KC_4,  KC_5,  KC_6,   KC_EQUAL,  _______, _______,  KC_F6,  KC_F7,   KC_F8,  KC_F9,   KC_F10,
-    KC_TRNS,  KC_1,  KC_2,  KC_3,   KC_MINUS,  _______, _______,  KC_F11, ALT_F12, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,  KC_TRNS, KC_PDOT, KC_TRNS, KC_TRNS, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS
+    KC_TRNS,  KC_1,  KC_2,  KC_3,   KC_MINUS,  _______, _______,  KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,  KC_0, KC_PDOT, KC_TRNS, KC_TRNS, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
 
 /* Symbols
@@ -118,7 +119,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, _______, _______, KC_MPLY,  KC_MUTE,  KC_MPRV,  KC_VOLD,   KC_MNXT
 ),
 
-/* Adjust (Lower + Raise)
+/* Function layer
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FROW] = LAYOUT_planck_grid(
+    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+),
+/* Nav
  *
  * ,-----------------------------------------------------------------------------------.
  * |      |      | PgUp | PgDn | Pscr |      |      |      |      |      |      |      |
@@ -138,3 +156,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 };
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    return true;
+}
